182d740565c13d91727f3546402f2d06732299e1
lipengzhou <lpzmail@163.com>
2020-01-06 10:08:56 +0800

    项目初始化-导入Vant

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载
+import {
+  Button,
+  Cell,
+  CellGroup
+} from 'vant'
+
+// 注册
+Vue.use(Button)
+  .use(Cell)
+  .use(CellGroup)
+// Vue.use(Button)
+// Vue.use(Cell)
+// Vue.use(CellGroup)
+
 Vue.config.productionTip = false
 
 new Vue({
