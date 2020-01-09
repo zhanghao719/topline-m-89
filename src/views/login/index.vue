8677c4ba61423e99f8d6066b854b6429277875b0
lipengzhou <lpzmail@163.com>
2020-01-09 12:23:26 +0800

    Token处理-存储Token

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -121,9 +121,10 @@ export default {
 
       // 3. 请求登录
       try {
-        const res = await login(user)
+        const { data } = await login(user)
 
-        console.log(res)
+        // 将登录成功获取到的用户 token 相关数据存储到 Vuex 容器
+        this.$store.commit('setUser', data.data)
 
         // 提示成功
         this.$toast.success('登录成功')
