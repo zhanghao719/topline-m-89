6647b45b97f2c6aa1faea5d2ad41998b47218352
lipengzhou <lpzmail@163.com>
2020-01-11 09:54:04 +0800

    个人中心-展示当前登录用户信息

--- src/api/user.js
+++ src/api/user.js
@@ -3,6 +3,7 @@
  */
 import request from '@/utils/request'
 
+// 用户登录
 export const login = data => {
   // 具体的请求代码
   return request({
@@ -17,9 +18,18 @@ export const login = data => {
   })
 }
 
+// 获取验证码
 export const getSmsCode = mobile => {
   return request({
     method: 'GET',
     url: `/app/v1_0/sms/codes/${mobile}`
+  })
+}
+
+// 获取当前登录用户信息
+export const getUserInfo = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user'
   })
 }
