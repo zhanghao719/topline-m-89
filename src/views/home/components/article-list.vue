7d99f29f4b466175e4597420799f1a545e270cdf
lipengzhou <lpzmail@163.com>
2020-01-11 12:41:53 +0800

    首页-展示文章列表

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -7,15 +7,17 @@
       @load="onLoad"
     >
       <van-cell
-        v-for="item in list"
-        :key="item"
-        :title="item"
+        v-for="(article, index) in list"
+        :key="index"
+        :title="article.title"
       />
     </van-list>
   </div>
 </template>
 
 <script>
+import { getArticlesByChannel } from '@/api/article'
+
 export default {
   name: 'ArticleList',
   components: {},
@@ -31,7 +33,8 @@ export default {
     return {
       list: [],
       loading: false,
-      finished: false
+      finished: false,
+      timestamp: null // 用于获取下一页数据的页码（时间戳）
     }
   },
   computed: {},
@@ -39,20 +42,27 @@ export default {
   created () {},
   mounted () {},
   methods: {
-    onLoad () {
-      // 异步更新数据
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-        // 加载状态结束
-        this.loading = false
+    async onLoad () {
+      // 1. 请求获取数据
+      const { data } = await getArticlesByChannel({
+        channel_id: this.channel.id, // 频道id
+        timestamp: this.timestamp || Date.now(), // 时间戳，请求新的推荐数据传当前的时间戳，请求历史推荐传指定的时间戳
+        with_top: 1 // 是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
+      })
 
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 500)
+      // 2. 把数据添加到列表中
+      const { results } = data.data
+      this.list.push(...results)
+
+      // 3. 停止本次的 loading
+      this.loading = false
+
+      // 4. 判断数据是否加载结束
+      if (results.length) {
+        this.timestamp = data.data.pre_timestamp // 更新获取下一页数据的时间戳
+      } else {
+        this.finished = true // 没有数据了，设置加载结束
+      }
     }
   }
 }
