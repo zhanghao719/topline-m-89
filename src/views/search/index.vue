ce02cd48208d44f68429660d08ece7fe6c3b85e6
lipengzhou <lpzmail@163.com>
2020-01-14 09:10:08 +0800

    文章搜索-处理页面显示状态

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="search-container">
     <!-- 搜索栏 -->
-    <form action="/">
+    <form class="search-form" action="/">
       <van-search
         v-model="searchContent"
         placeholder="请输入搜索关键词"
         show-action
         @search="onSearch"
         @cancel="onCancel"
+        @focus="isSearchResultShow = false"
       />
     </form>
     <!-- /搜索栏 -->
 
+    <!-- 搜索结果 -->
+    <search-result v-if="isSearchResultShow" />
+    <!-- /搜索结果 -->
+
     <!-- 联想建议 -->
-    <van-cell-group>
+    <van-cell-group v-else-if="searchContent">
       <van-cell icon="search" title="单元格" />
       <van-cell icon="search" title="单元格" />
       <van-cell icon="search" title="单元格" />
@@ -24,13 +29,13 @@
     <!-- /联想建议 -->
 
     <!-- 历史记录 -->
-    <van-cell title="历史记录">
-      <van-icon name="delete" />
-      <span>全部删除</span>
-      &nbsp;&nbsp;
-      <span>完成</span>
-    </van-cell>
-    <van-cell-group>
+    <van-cell-group v-else>
+      <van-cell title="历史记录">
+        <van-icon name="delete" />
+        <span>全部删除</span>
+        &nbsp;&nbsp;
+        <span>完成</span>
+      </van-cell>
       <van-cell title="单元格">
         <van-icon name="close"></van-icon>
       </van-cell>
@@ -48,35 +53,22 @@
       </van-cell>
     </van-cell-group>
     <!-- /历史记录 -->
-
-    <!-- 搜索结果 -->
-    <van-list
-      v-model="loading"
-      :finished="finished"
-      finished-text="没有更多了"
-      @load="onLoad"
-    >
-      <van-cell
-        v-for="item in list"
-        :key="item"
-        :title="item"
-      />
-    </van-list>
-    <!-- /搜索结果 -->
   </div>
 </template>
 
 <script>
+import SearchResult from './components/search-result'
+
 export default {
   name: 'SearchPage',
-  components: {},
+  components: {
+    SearchResult
+  },
   props: {},
   data () {
     return {
       searchContent: '', // 搜索内容
-      list: [],
-      loading: false,
-      finished: false
+      isSearchResultShow: false // 是否展示搜索结果
     }
   },
   computed: {},
@@ -86,28 +78,30 @@ export default {
   methods: {
     onSearch () {
       console.log('onSearch')
+
+      // 展示搜索结果
+      this.isSearchResultShow = true
     },
     onCancel () {
       console.log('onCancel')
-    },
-
-    onLoad () {
-      // 异步更新数据
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-        // 加载状态结束
-        this.loading = false
-
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 500)
     }
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.search-container {
+  padding-top: 54px;
+  // 让搜索栏固定在顶部
+  .search-form {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1;
+  }
+  .van-search__action {
+    color: #fff;
+  }
+}
+</style>
