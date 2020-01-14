39a0b7b5ac28f1b2aa29a66dec34bae48dd5db8a
lipengzhou <lpzmail@163.com>
2020-01-14 11:31:08 +0800

    文章搜索-创建组件并配置路由

---
+++ src/views/article/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="article-container">文章详情</div>
+</template>
+
+<script>
+export default {
+  name: 'ArticlePage',
+  components: {},
+  props: {
+    articleId: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped></style>
